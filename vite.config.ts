49ac749e02baf685805d13d4973768fe514898f1
ak220193 <mail2meak22frcrio@gmail.com>
2025-09-11 18:15:03 +0530

    changes 4 bug issues

--- vite.config.ts
+++ vite.config.ts
@@ -18,9 +18,5 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
   },
 }));
